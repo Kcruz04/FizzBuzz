54e4e393e7f124c02ff3bc2e1dd15bfe4638be85
Kcruz04 <kencruz515@gmail.com>
2023-03-27 14:06:04 -1000

    fizzbuzz_edit

--- FizzBuzz.Loop.js
+++ FizzBuzz.Loop.js
@@ -1,16 +1,17 @@
 
 function fizzbuzz(){
     for (var i = 1; i < 101; i++){
-        if(i %3==0){
+        if(i%3==0 && i%5==0){
+        console.log("fizz buzz")
+        }
+        else if(i %3==0){
             console.log("fizz")
         }
         else if(i%5==0){
             console.log("buzz")
+            
         }
-        else if(i%3==0 && i%5==0){
-            console.log("fizzbuzz")
-        }
-        else{ console.log(i)}
+        else { console.log(i)}
     }
 
 }
